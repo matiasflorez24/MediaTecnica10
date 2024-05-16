a32a45a252cb6e05b11f7d6329d58a7204438f25
matiasflorez24 <matiasalzate100@gmail.com>
2024-05-16 15:47:51 -0500

    Programa Participantes Los Simpson
    Procedimiento para mostrar nombres de los participantes

---
+++ ProgramaParticipantes.cs
@@ -0,0 +1,25 @@
+/******************************************************************************
+Autor: Andres Rojas - Matias Florez
+Fecha: 2024-05-16
+
+Los Simpson estan organizando un torneo de videojuegos y necesitan un programa
+en C# para mostrar los nombres de los participantes. completa el espacio en 
+blanco para imprimir los nombres en la consola:
+
+*******************************************************************************/
+using System;
+class program
+{
+  static void Main() 
+  {
+      string participante1 = "Bart";
+      string participante2 = "Lisa";
+      string participante3 = "Milhouse";
+      string participante4 = "Nelson";
+      
+        Console.WriteLine("El participante1 es: " + participante1);
+        Console.WriteLine("El participante2 es: " + participante2);
+        Console.WriteLine("El participante3 es: " + participante3);
+        Console.WriteLine("El participante4 es: " + participante4);
+  }
+}
