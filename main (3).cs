405d32753876ae5a0ee1875d716f26095ad1c62b
matiasflorez24 <matiasalzate100@gmail.com>
2024-05-29 17:00:32 -0500

    trabajo edad y preferencias
    terminar trabajo

---
+++ main (3).cs
@@ -0,0 +1,32 @@
+/******************************************************************************
+Autor:          Julián A. Peña
+Fecha:          29-05-2024
+Descripción:    Ejercicio estructura de decisión múltiple
+Se desea un programa que ayude diciendo 
+las preferencias según el género:
+- Si es un hombre menor de edad sus preferencias son:
+    Figuras de acción, Caricaturas, Series TV
+- Si es un hombre mayor de edad sus preferencias son:
+    cine, picnic y filantropía
+- Si es una mujer menor de edad sus preferencias son:
+    Muñecas, Maquillaje, Series TV
+- Si es una mujer mayor de edad sus preferencias son:
+    Maquillaje, Series TV, Cine, filantropía
+*******************************************************************************/
+using System;
+class Programa {
+  static void Main(string[] args) {
+    Console.WriteLine("preferencias según el genero (Hombre/Mujer)");
+    Console.Write("ingrese el genero: ");
+     string genero = Console.ReadLine());
+     
+     Console.Write("ingrese su edad: ");
+     int edad = Int32.Parse(Console.ReadL 18ine());
+     
+     Console.WriteLine("sus preferencias son: ");
+     if (genero = "hombre"){
+  }
+  
+  if (edad < 18)
+  
+}
