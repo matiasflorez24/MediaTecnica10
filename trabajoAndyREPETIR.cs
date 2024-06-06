ee1acb3ae29f4f261b442bd2b8cab3da975c4eaa
matiasflorez24 <matiasalzate100@gmail.com>
2024-06-06 16:28:55 -0500

    TrabajoAndyREPETIR

---
+++ trabajoAndyREPETIR.cs
@@ -0,0 +1,59 @@
+/******************************************************************************
+
+Autor:          Matias Florez A.
+Fecha:          06-06-2024
+Descripción:    Toy Story
+     Los juguetes de Andy están 
+     jugando a las escondidas. Pide al 
+     usuario las ubicaciones donde buscar 
+     y usa estructuras "si" para determinar 
+     si los juguetes están en la ubicación 
+     correcta o no. Reportar la cantidad 
+     total de juguetes que fueron encontrados 
+     al finalizar 4 rondas.
+
+*******************************************************************************/
+using System;
+
+class HelloWorld {
+    static void Main() {
+        // Variables
+        int juguetesEncontrados = 0;
+
+        // Estructura repetitiva o cíclica
+        int i = 0;
+        while (i <= 5)
+            
+            // Preguntar ubicaciones al usuario
+            Console.WriteLine("Ronda " + (i+1) + ": ¿Dónde deberíamos buscar?");
+            Console.WriteLine("Opciones: habitación, sala de estar, cocina, baño, atico");
+            string ubicacion = Console.ReadLine().ToLower();
+            
+            // Validar ubicaciones con estructura IF
+            if (ubicacion == "habitacion") {
+                Console.WriteLine("¡Encontramos dos juguete en la habitacion!");
+                juguetesEncontrados++;
+                juguetesEncontrados++;
+            }
+            else if (ubicacion == "sala de estar") {
+                Console.WriteLine("No hay juguetes en la sala de estar.");
+            }
+            else if (ubicacion == "cocina") {
+                Console.WriteLine("No hay juguetes en la cocina.");
+            }
+            else if (ubicacion == "baño") {
+                Console.WriteLine("¡Encontramos un juguete en el baño!");
+                juguetesEncontrados++;
+            }
+            else if (ubicacion == "atico") {
+                Console.WriteLine("No hay juguetes en el atico");
+            }
+            else {
+                Console.WriteLine("Ubicación no válida.");
+            }
+        }
+        
+        // Mostrar juguetes encontrados
+        Console.WriteLine("Total de juguetes encontrados: " + juguetesEncontrados);
+    }
+}
